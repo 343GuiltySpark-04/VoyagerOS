87004615fe7d9aed52149553ec83c2293fce9896
Tristan Adams <killerdragonxs@gmail.com>
2022-09-23 14:28:32 -0400

    dfc

--- kernel/arch/i386/status_printer.asm
+++ kernel/arch/i386/status_printer.asm
@@ -1,8 +1,9 @@
 [bits 32]
 
 global e9_port_test
-
-
+global nominal_boot
+global interrupt_check_fail
+global interrupts_enabled
 
 e9_port_test:
 mov dx,0xe9
@@ -22,4 +23,33 @@ mov eax,0x2A
 out dx,eax
 mov eax,0x30
 out dx,eax
+out dx,eax
+mov eax,0x31
+out dx,eax
+mov eax,0x2A
+out dx,eax
+
+
+interrupt_check_fail:
+mov dx,0xe9
+mov eax,0x2A
+out dx,eax
+mov eax,0x30
+out dx,eax
+out dx,eax
+mov eax,0x32
+out dx,eax
+mov eax,0x2A
+out dx,eax
+
+interrupts_enabled:
+mov dx,0xe9
+mov eax,0x2A
+out dx,eax
+mov eax,0x30
+out dx,eax
+out dx,eax
+mov eax,0x34
+out dx,eax
+mov eax,0x2A
 out dx,eax
