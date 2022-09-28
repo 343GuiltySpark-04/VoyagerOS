911b28ba24c80db3e649120f9259e10aa50d7bf5
Tristan Adams <killerdragonxs@gmail.com>
2022-09-27 21:00:44 -0400

    Update kheap.c

--- libc/stdlib/kheap.c
+++ libc/stdlib/kheap.c
@@ -16,21 +16,21 @@ typedef struct _KHEAPBM {
  
 void k_heapBMInit(KHEAPBM *heap);
 int k_heapBMAddBlock(KHEAPBM *heap, unintptr addr, u32int size, u32int bsize);
-int k_heapBMAddBlockEx(KHEAPBM *heap, unintptr addr, u32int size, uint32 bsize, KHEAPBLOCKBM *b, uint8 *bm, uint8 isBMInside);
-void *k_heapBMAlloc(KHEAPBM *heap, uint32 size);
+int k_heapBMAddBlockEx(KHEAPBM *heap, unintptr addr, u32int size, u32int bsize, KHEAPBLOCKBM *b, u8int *bm, u8int isBMInside);
+void *k_heapBMAlloc(KHEAPBM *heap, u32int size);
 void k_heapBMFree(KHEAPBM *heap, void *ptr);
-uintptr k_heapBMGetBMSize(uintptr size, uint32 bsize);
-void *k_heapBMAllocBound(KHEAPBM *heap, uint32 size, uint32 mask);
-void k_heapBMSet(KHEAPBM *heap, uintptr ptr, uintptr size, uint8 rval);
+unintptr k_heapBMGetBMSize(unintptr size, u32int bsize);
+void *k_heapBMAllocBound(KHEAPBM *heap, u32int size, u32int mask);
+void k_heapBMSet(KHEAPBM *heap, unintptr ptr, unintptr size, u8int rval);
  
 void k_heapBMInit(KHEAPBM *heap) {
 	heap->fblock = 0;
 }
  
-int k_heapBMAddBlock(KHEAPBM *heap, uintptr addr, uint32 size, uint32 bsize) {
+int k_heapBMAddBlock(KHEAPBM *heap, unintptr addr, u32int size, u32int bsize) {
 	KHEAPBLOCKBM			*b;
-	uintptr					bmsz;
-	uint8					*bm;
+	unintptr					bmsz;
+	u8int					*bm;
  
 	b = (KHEAPBLOCKBM*)addr;
 	bmsz = k_heapBMGetBMSize(size, bsize);
